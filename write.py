5713cf839bdad036a0662bdb4d022d4e242d67cb
jojo <jojo@jojo.fr>
2019-02-28 20:13:09 +0100

    alpha output

--- write.py
+++ write.py
@@ -1,3 +1,17 @@
-def write(output, filename):
-    with open(filename, 'w') as f:
-        f.write('\n')
+import output_slides
+
+
+class OutputWriter:
+    def __init__(self, filename, outputSlidesCol):
+        self.filename = filename
+        self.outputSlidesCol = outputSlidesCol
+
+    def write(self):
+        with open(self.filename, 'w') as f:
+            f.write(len(self.outputSlidesCol))
+            f.write("\n")
+            for slide in self.outputSlidesCol:
+                f.write('%(num)' % slide[0])
+                if slide[1] is not None:
+                    f.write(' %(num)', slide[1])
+                f.write("\n")
